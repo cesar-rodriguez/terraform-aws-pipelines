ad5c95ff94beadd32b9e0628e28e9055c8136eab
Cesar Rodriguez <therasec@gmail.com>
2018-01-06 21:32:11 -0500

    adds better error handling

--- terraform-pr-pipeline/pipeline-delete/pipeline-delete.py
+++ terraform-pr-pipeline/pipeline-delete/pipeline-delete.py
@@ -14,30 +14,59 @@ logging.getLogger('botocore').setLevel(logging.ERROR)
 # Boto clients
 codepipeline = boto3.client('codepipeline')
 codebuild = boto3.client('codebuild')
+s3 = boto3.client('s3')
 
 # Global vars
 bucket = os.environ['BUCKET_NAME']
 project_name = os.environ['PROJECT_NAME']
 
 
+def object_exists(key):
+    """
+    Determines if objet exists in S3 bucket
+    """
+    try:
+        s3.get_object(Bucket=bucket, Key=key)
+    except s3.exceptions.NoSuchKey:
+        return False
+    return True
+
+
+def delete_pipeline(pipeline_name):
+    """
+    Deletes the specified AWS CodePipeline pipeline
+    """
+    logger.info('Deleting pipeline: {}'.format(pipeline_name))
+    codepipeline.delete_pipeline(name=pipeline_name)
+
+
+def delete_codebuild_project(project_name):
+    """
+    Deletes the specified AWS CodeBuild project
+    """
+    logger.info('Deleting codebuild project: {}'.format(
+        project_name))
+    codebuild.delete_project(name=project_name)
+
+
 def lambda_handler(event, context):
     """
-    Deletes pipeline resources associated to deleted s3 object
+    Deletes pipeline resources associated to S3 object that no longer exists
     """
-    pr_number = event['Records'][0]['s3']['object']['key'].split('/')[0]
+    repo_object = event['Records'][0]['s3']['object']['key']
 
-    logger.debug('Deleting pipeline')
-    codepipeline.delete_pipeline(name='{}-terraform-pr-pipeline-{}'.format(
-        project_name, pr_number))
+    if 'repo.zip' in repo_object and not object_exists(repo_object):
+        logger.info('Object no longer exists at: {}'.format(repo_object))
+        pr_number = repo_object.split('/')[0]
 
-    logger.debug('Deleting terraform fmt codebuild project')
-    codepipeline.delete_project(name='{}-terraform-pr-fmt-{}'.format(
-        project_name, pr_number))
+        delete_pipeline('{}-terraform-pr-pipeline-{}'.format(
+            project_name, pr_number))
 
-    logger.debug('Deleting terrascan codebuild project')
-    codepipeline.delete_project(name='{}-terraform-pr-terrascan-{}'.format(
-        project_name, pr_number))
+        delete_codebuild_project('{}-terraform-pr-fmt-{}'.format(
+            project_name, pr_number))
 
-    logger.debug('Deleting terraform plan codebuild project')
-    codepipeline.delete_project(name='{}-terraform-pr-plan-{}'.format(
-        project_name, pr_number))
+        delete_codebuild_project('{}-terraform-pr-terrascan-{}'.format(
+            project_name, pr_number))
+
+        delete_codebuild_project('{}-terraform-pr-plan-{}'.format(
+            project_name, pr_number))
